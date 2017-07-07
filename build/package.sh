c3d4327d5de4dd9fdc65e461d31ab8995bf8645b
kybernetikos <me@kybernetikos.com>
2017-07-07 12:54:56 +0100

    packaging

--- build/package.sh
+++ build/package.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+cd "$(dirname "$0")"
+
+source ./env.sh
+
 rm -rf tmp
+rm ../dist/*
+
+version=$( sed -n 's/.*"version": "\(.*\)",/\1/p' ../manifest.json )
+parts=( ${version//./ } )
+((parts[2]++))
+newVersion="${parts[0]}.${parts[1]}.${parts[2]}"
+echo "Current version: $version"
+echo "New version: $newVersion"
+
+sed -i -e "s/\"version\": \"$version\",/\"version\": \"$newVersion\",/" ../manifest.json
 
 mkdir -p tmp/sinkless
 cp -r ../img tmp/sinkless
@@ -11,6 +25,6 @@ cp ../styles.css tmp/sinkless
 
 "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --pack-extension=./tmp/sinkless --pack-extension-key=./sinkless.pem
 
-rm ../dist/*
+cp ./tmp/sinkless.crx ../dist
 
-cp tmp/sinkless.crx ../dist
+web-ext sign --api-key=$AMO_JWT_ISSUER --api-secret=$AMO_JWT_SECRET --source-dir ..  --artifacts-dir ../dist --ignore-files "build" "dist" "img/screenshot.png" ".idea" ".git" ".gitignore" "genpass.js" "README.md"
